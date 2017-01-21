02548b1b68225f0d8d55b9c32dcd393ea6fb0ee9
adeto <adeto@users.noreply.github.com>
2017-01-21 13:29:52 +0200

    04. Time +15 minutes

---
+++ 15Mins.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _15.Time__15_Minutes
+{
+    class Program
+    {
+        static void Main()
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+
+            var sum = ((num1 * 60) + num2 + 15);
+            var resulthour = sum / 60;
+            if (resulthour >= 24)
+            {
+                resulthour -= 24;
+            }
+            var resultmin = sum % 60;
+
+            Console.WriteLine("{0}:{1:00}", resulthour, resultmin);
+       
+
+        }
+    }
+}
