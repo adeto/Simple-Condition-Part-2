e5ef46dd91e1ab0feca4aa24b3f2149cd62d6dab
adeto <adeto@users.noreply.github.com>
2017-01-21 13:29:23 +0200

    03. Area of Figures

---
+++ Area_Figures.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace _14.Area_Of_Figures
+{
+    class Area_Figures
+    {
+        static void Main()
+        {
+            string figure = Console.ReadLine().ToLower();
+            var num = double.Parse(Console.ReadLine());
+
+            if (figure == "square")
+            {
+                var areaSquare = num * num;
+                Console.WriteLine(areaSquare);
+            }
+            else if (figure == "rectangle")
+            {
+                var h = double.Parse(Console.ReadLine());
+                var areaRectangle = num * h;
+                Console.WriteLine(areaRectangle);
+            }
+            else if (figure == "circle")
+            {
+                var r = num * num;
+                var result = r * Math.PI;
+                var areaCircle = result;
+                Console.WriteLine(areaCircle);
+            }
+            else if (figure == "triangle")
+            {
+                var b = double.Parse(Console.ReadLine());
+                var areaTriangle = (num * b) / 2;
+                Console.WriteLine(areaTriangle);
+
+            }
+        }
+    }
+}
