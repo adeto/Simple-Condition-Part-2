07851d428318ac98847f9eb9e8466ef5cc4de0ad
adeto <adeto@users.noreply.github.com>
2017-01-21 13:30:27 +0200

    05. Even or Odd

---
+++ EvenOrOdd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Even_or_Odd
+{
+    class EvenOrOdd
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
