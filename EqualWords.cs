faa3d1a127829809605ee3c7d6711d85cb78adc8
adeto <adeto@users.noreply.github.com>
2017-01-21 13:28:35 +0200

    01. Equal Words

---
+++ EqualWords.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _12.Equal_Words
+{
+    class EqualWords
+    {
+        static void Main()
+        {
+            string firstWord = Console.ReadLine().ToLower();
+            string secondWord = Console.ReadLine().ToLower();
+
+            if (firstWord == secondWord)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
