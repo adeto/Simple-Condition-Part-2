614eb81b1bb725b7984c0df9f4a1c83120084afe
adeto <adeto@users.noreply.github.com>
2017-01-21 13:31:01 +0200

    Excellent Result

---
+++ Excellent Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Excellent_Result
+{
+    class ExcellentGrade
+    
+    {
+        static void Main()
+        {
+            var number = double.Parse(Console.ReadLine());
+
+            if (number >= 5.50)
+
+            {
+                Console.WriteLine("Excellent!");
+
+
+            }
+        }
+    }
+}
