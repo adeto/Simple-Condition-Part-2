21c469fea7fbeb9159e1207d56253e9626843a02
adeto <adeto@users.noreply.github.com>
2017-01-21 13:31:41 +0200

    Excellent or Not

---
+++ ExcelentOrNot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Excellent_or_Not
+{
+    class ExcelentOrNot
+    {
+        static void Main()
+        {
+            var number = double.Parse(Console.ReadLine());
+
+            if (number >= 5.50)
+
+            {
+                Console.WriteLine("Excellent!");
+            }
+
+            else
+            { 
+                Console.WriteLine("Not Excellent.");
+
+            }
+        }
+    }
+}
